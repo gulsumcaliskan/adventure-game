c995669cecd8bbcd44822f7762ad26e9d6be19ab
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-05-11 20:06:18 +0300

    Update BattleLoc.java

--- BattleLoc.java
+++ BattleLoc.java
@@ -4,7 +4,7 @@ public class BattleLoc extends Location {
     private String award;
     private int maxMonster;
 
-    public BattleLoc(Monster monster, Player player, String name, String award, int maxMonster){
+    public BattleLoc(Player player, String name, Monster monster, String award, int maxMonster){
         super(player, name);
         this.monster = monster;
         this.award = award;
@@ -14,7 +14,7 @@ public class BattleLoc extends Location {
     @Override
 
     public boolean onLocation(){
-        
+        return true;
     }
 
 
