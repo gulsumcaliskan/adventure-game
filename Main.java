7c976529b9701377e48d4a8a313205aed777f57d
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-05-11 12:15:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args){
+        
+//        Player p1 = new Player("Harry", 100, 80, 100);
+//        Samurai s1 = new Samurai();
+//        Archer a1 = new Archer();
+//       Knight k1 = new Knight();
+
+//        System.out.println(s1.getDamage());
+//        System.out.println(a1.getMoney());
+ //       System.out.println(k1.getHealth());
+ //       Player p1 = new Player();
+ //       p1.printInfo();
+
+        Game game = new Game();
+        game.start();
+    }
+}
